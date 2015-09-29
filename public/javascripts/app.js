5eda1ef47795b033179bd6fc463d7bd1135f9f39
Nick Mattei <nmattei89@gmail.com>
2015-09-29 16:10:52 -0600

    getting partials bootstrapped

---
+++ public/javascripts/app.js
@@ -0,0 +1,25 @@
+var app = angular.module("grockr", ['ui.router','angularModalService'])
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/home");
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "partials/splash.html"
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "partials/about.html"
+    })
+
+});
+
+// app.run(function ($rootScope, $state, AuthService) {
+//   $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
+//     if (toState.authenticate && !AuthService.isAuthenticated()) {
+//       $state.transitionTo("not-authenticated");
+//       event.preventDefault();
+//     }
+//
+//   })
+// })
