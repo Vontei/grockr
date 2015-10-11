52d9eac4d02a28b020d4ea61a9b65a10275a2e41
Nick Mattei <nmattei89@gmail.com>
2015-10-11 16:16:08 -0600

    nested views

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,19 +2,63 @@ var app = angular.module("grockr", ['ui.router','angularModalService'])
 app.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
-    .state('splash', {
-      url: "/splash",
-      templateUrl: "partials/splash.html",
-      controller: "xhrController",
-    })
-    .state('dash', {
-      url: "/dash",
-      templateUrl: "partials/dash.html",
-      controller: 'yahooController'
-    })
-    .state('landing', {
+    .state('home', {
       url: "/",
       templateUrl: "partials/landing.html",
+      // controller: "homeController"
+    })
+    .state('home.dash', {
+      url: "/dash",
+      templateUrl: "partials/dash.html",
+      // controller: 'dashController'
+    })
+    .state('home.dash.level1', {
+      url: "/level1",
+      templateUrl: "partials/level1.html",
+      // controller: "level1Controller"
+
+    })
+    .state('home.dash.level2', {
+      url: "/level2",
+      templateUrl: "partials/level2.html",
+      // controller: "level2Controller"
+
+    })
+    .state('home.dash.level3', {
+      url: "/level3",
+      templateUrl: "partials/level3.html",
+      // controller: "level3Controller"
+
+    })
+    .state('home.dash.level4', {
+      url: "/level4",
+      templateUrl: "partials/level4.html",
+      // controller: "level4Controller"
+    })
+    .state('splash', {
+        url: "/splash",
+        templateUrl: "partials/splash.html",
+        // controller: "splashController",
+    })
+    .state('home.dash.profile', {
+        url: "/profile",
+        templateUrl: "partials/profile.html",
+        // controller: "profileController",
+    })
+    .state('home.dash.account', {
+        url: "/account",
+        templateUrl: "partials/profile.html",
+        // controller: "accountController",
+    })
+    .state('home.dash.orders', {
+        url: "/orders",
+        templateUrl: "partials/orders.html",
+        // controller: "ordersController",
+    })
+    .state('home.dash.members', {
+        url: "/members",
+        templateUrl: "partials/members.html",
+        // controller: "ordersController",
     })
   // $urlRouterProvider.otherwise("home");
 
